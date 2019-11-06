e63181ae5d8028ca7b0a9622f9f125d238eac962
Michael Fosker <mikefosker@Michaels-MacBook-Pro.local>
2019-11-06 12:14:37 +0000

    Update Goodbye Lambda to use async, much neater

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 A trimmed down version of https://github.com/sw-yx/create-react-app-lambda-typescript,
 with all the react stuff stripped out.
+
+build or run with yarn
--- src/lambda/goodbye.ts
+++ src/lambda/goodbye.ts
@@ -1,25 +1,10 @@
 import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda';
 
-interface HelloResponse {
-  statusCode: number;
-  body: string;
+const handler = async (event: APIGatewayEvent, context:Context) => {
+  return {
+    statusCode: 200,
+    body: "Goodbye, World"
+  }
 }
 
-const handler: Handler = (
-  event: APIGatewayEvent,
-  context: Context,
-  callback: Callback
-) => {
-  const params = event.queryStringParameters;
-  const response: HelloResponse = {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: `Goodbye world ${Math.floor(Math.random() * 10)}`,
-      params
-    })
-  };
-
-  callback(undefined, response);
-};
-
-export { handler };
+export {handler}
